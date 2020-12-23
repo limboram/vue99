688fb63bd8009ac7da61acd12ec558a63d17ad37
limboram <qhfka8773@naver.com>
2020-12-24 00:57:46 +0900

    2020.12.24.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrap">
+      <main id="main">
+        <section id="mainCont">
+          <div class="mainCont">
+            <div v-for="text in texts" v-bind:key="text.title">
+              {{ text.title }}
+            </div>
+          </div>
+        </section>
+      </main>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      texts: [
+        { title: 'FORGIVENESS' },
+        { title: 'IS BETTER' },
+        { title: 'THAN' },
+        { title: 'REVENGE.' }
+      ]
+    }
   }
 }
 </script>
+
+<style lang="scss">
+  .mainCont > div:nth-child(1){
+    background: #fff;
+    color: #000;
+    display: inline-block;
+  }
+</style>
